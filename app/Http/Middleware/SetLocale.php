aac2f8994c4856527d097f94ec2f41e55032f2dd
Luisa <luisa.nunez150@gmail.com>
2021-06-10 12:31:15 -0600

    Middleware to administrate language selection

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::check()) {
+            App::setLocale(session('locale'));
+        }
+
+        return $next($request);
+    }
+}
