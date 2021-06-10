f941b09fba54f259a742c499fe98b4887c759404
Luisa <luisa.nunez150@gmail.com>
2021-06-10 12:31:40 -0600

    Translation of Laravel app and custom fields to spanish

---
+++ resources/lang/es/custom.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Products' => 'Productos',
+    'Brands' => 'Marcas',
+    'Add product' => 'Agrega un producto',
+    'Name' => 'Nombre',
+    'Model No' => 'Número de modelo',
+    'Brand' => 'Marca',
+    'Options' => 'Opciones',
+    'Add brand' => 'Agrega una marca',
+    'Website' => 'Sitio web',
+    'Save' => 'Guardar',
+    'Choose brand...' => 'Escoge una marca',
+];
